3a27f472eb8167eba76de7fa663c99a8a0569e69
Bartosz Jarmuz <Bartosz.Jarmuz@EntainGroup.com>
2022-06-13 21:38:34 +0200

    add settings descriptions

--- src/TinyTODO.Core/Persistence/Settings.cs
+++ src/TinyTODO.Core/Persistence/Settings.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 using TinyTODO.Core.Contracts;
 
 namespace TinyTODO.Core
@@ -18,8 +19,13 @@ namespace TinyTODO.Core
         private static readonly Lazy<ISettings> lazy = new Lazy<ISettings>(() => new Settings());
         public static ISettings Instance { get { return lazy.Value; } }
 
+        [Description("Set to true to see completed tasks on the list.")]
         public bool ShowCompleted { get => _dataAccess.Get<bool>(Keys.ShowCompleted, false); set => _dataAccess.Set(Keys.ShowCompleted, value); }
+        
+        [Description("When true, the app will be hidden to System Tray when Close button is clicked.")]
         public bool CloseToTray { get => _dataAccess.Get<bool>(Keys.CloseToTray, false); set => _dataAccess.Set(Keys.CloseToTray, value); }
+
+        [Description("When true, the app will be hidden to System Tray when Minimize button is clicked.")]
         public bool MinimizeToTray { get => _dataAccess.Get<bool>(Keys.MinimizeToTray, true); set => _dataAccess.Set(Keys.MinimizeToTray, value); }
 
         private static class Keys
